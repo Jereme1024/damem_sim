c8d16bfb3d7bfba65d4d7f257de16c7f89ffe0ea
thestyle <thestyle1202@gmail.com>
2014-06-08 04:00:05 +0800

    refactory

--- damemory.hpp
+++ damemory.hpp
@@ -45,17 +45,19 @@ private:
 	std::map<std::string, Page *> pages_;
 	Scheduler<Page> *page_scheduler_;
 	int cnt_access_;
+	int cnt_swap_;
 
 public:
 	Page_reposity(Scheduler<Page> *page_scheduler)
 		: pages_()
 		, page_scheduler_(page_scheduler)
 		, cnt_access_(0)
+		, cnt_swap_(0)
 	{}
 
-	void add(std::string name, int index)
+	void allocate(std::string prefix, int index)
 	{
-		std::string name_new = name + "." + std::to_string(index);
+		std::string name_new = prefix + "." + std::to_string(index);
 		Page *page_new = new Page(name_new);
 
 		pages_.insert(std::pair<std::string, Page *>(name_new, page_new));
@@ -65,6 +67,7 @@ public:
 		} else {
 			page_scheduler_->evict_one();
 			page_scheduler_->on_schedule(page_new);
+			cnt_swap_ += 1;
 		}
 	}
 
@@ -78,6 +81,8 @@ public:
 		} else {
 			page_scheduler_->evict_one();
 			page_scheduler_->on_schedule(pages_[name]);
+
+			cnt_swap_ += 1;
 		}
 	}
 
@@ -86,37 +91,40 @@ public:
 		return cnt_access_;
 	}
 
-	int get_cnt_swap(...)
+	int get_cnt_swap()
 	{
-		return -1;
+		return cnt_swap_;
 	}
 };
 
 
+template <template <class> class Scheduler_type>
 class Page_manager
 {
 private:
-	config2d config_da_page;
-	config2d config_da_page_real;
+	config2d config_num_;
+	config2d config_size_;
 
-	std::list<Page> online;
-	std::list<Page> offline;
+	Scheduler<Page> *page_scheduler_;
+	Page_reposity page_reposity_;
 
 public:
-	Page_manager(config2d &config)
-		: config_da_page(config)
-		, config_da_page_real(1, 1)
+	Page_manager(config2d &config_num, config2d &config_size)
+		: config_num_(config_num)
+		, config_size_(config_size)
+		, page_scheduler_(new Scheduler_type<Page>(config_num.height * config_num.width))
+		, page_reposity_(page_scheduler_)
 	{}
 
-	void allocate(std::string name_data, int h, int w, int size_data)
+	void allocate(std::string prefix_name, int h, int w, int size_data)
 	{
-		const int num_row = config_da_page_real.height / h;
-		const int num_col = config_da_page_real.width / w;
+		const int num_row = config_size_.height / h;
+		const int num_col = config_size_.width / w;
 		const int num_total = num_row * num_col;
 
 		for (int i = 0; i < num_total; i++)
 		{
-			std::string name_new = name_data + "." + std::to_string(i);
+			page_reposity_.allocate(prefix_name, i);
 		}
 	}
 };
@@ -171,8 +179,5 @@ public:
 	{
 	}
 
-	void free(...)
-	{
-	}
 };
 #endif
