64a4341660d931dc9def21fe505ad76f9b6e10d2
thestyle <thestyle1202@gmail.com>
2014-06-08 03:31:34 +0800

    scheduler LRU test driver

--- damemory.hpp
+++ damemory.hpp
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <string>
+#include <map>
+#include <algorithm>
+
+#include "scheduler.hpp"
 
 struct config2d
 {
@@ -12,18 +17,79 @@ struct config2d
 
 	config2d(const int h, const int w) : height(h), width(w)
 	{}
+
+	config2d(config2d &c) : height(c.height), width(c.width)
+	{}
 };
 
 
 struct Page
 {
 	std::string name_;
-	bool is_online_;
 
-	Page(std::string name, bool is_online)
+	Page(std::string name)
 		: name_(name)
-		, is_online_(is_online)
 	{}
+
+
+	~Page()
+	{
+		std::cerr << "Page destructer!";
+	}
+};
+
+
+class Page_reposity
+{
+private:
+	std::map<std::string, Page *> pages_;
+	Scheduler<Page> *page_scheduler_;
+	int cnt_access_;
+
+public:
+	Page_reposity(Scheduler<Page> *page_scheduler)
+		: pages_()
+		, page_scheduler_(page_scheduler)
+		, cnt_access_(0)
+	{}
+
+	void add(std::string name, int index)
+	{
+		std::string name_new = name + "." + std::to_string(index);
+		Page *page_new = new Page(name_new);
+
+		pages_.insert(std::pair<std::string, Page *>(name_new, page_new));
+		if (!page_scheduler_->is_full())
+		{
+			page_scheduler_->on_schedule(page_new);
+		} else {
+			page_scheduler_->evict_one();
+			page_scheduler_->on_schedule(page_new);
+		}
+	}
+
+	void access(std::string name)
+	{
+		cnt_access_ += 1;
+
+		if (page_scheduler_->is_hit(pages_[name]))
+		{
+			page_scheduler_->update(pages_[name]);
+		} else {
+			page_scheduler_->evict_one();
+			page_scheduler_->on_schedule(pages_[name]);
+		}
+	}
+
+	int get_cnt_access()
+	{
+		return cnt_access_;
+	}
+
+	int get_cnt_swap(...)
+	{
+		return -1;
+	}
 };
 
 
@@ -31,6 +97,7 @@ class Page_manager
 {
 private:
 	config2d config_da_page;
+	config2d config_da_page_real;
 
 	std::list<Page> online;
 	std::list<Page> offline;
@@ -38,9 +105,20 @@ private:
 public:
 	Page_manager(config2d &config)
 		: config_da_page(config)
+		, config_da_page_real(1, 1)
 	{}
 
-	
+	void allocate(std::string name_data, int h, int w, int size_data)
+	{
+		const int num_row = config_da_page_real.height / h;
+		const int num_col = config_da_page_real.width / w;
+		const int num_total = num_row * num_col;
+
+		for (int i = 0; i < num_total; i++)
+		{
+			std::string name_new = name_data + "." + std::to_string(i);
+		}
+	}
 };
 
 
--- test.cpp
+++ test.cpp
@@ -1,10 +1,14 @@
 #include "damemory.hpp"
 #include "damemory_test.hpp"
+#include "scheduler_test.hpp"
 
 int main()
 {
 	DAmemory_test_create damemory_test_create;
 	damemory_test_create.main();
+
+	Scheduler_test<Scheduler_LRU, Page> scheduler_test;
+	scheduler_test.main();
 
 	return 0;
 }
