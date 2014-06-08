9fa1ad008b46cdf15beef5c28cd1bc119ae50827
thestyle <thestyle1202@gmail.com>
2014-06-08 14:53:45 +0800

    add scheduler_*.hpp for last commit

---
+++ scheduler.hpp
@@ -0,0 +1,73 @@
+#ifndef __SCHEDULER_HPP__
+#define __SCHEDULER_HPP__
+
+template <class Struct_type>
+class Scheduler
+{
+public:
+
+	virtual bool is_full() = 0;
+	virtual bool is_hit(Struct_type *) = 0;
+	virtual void update(Struct_type *) = 0;
+	virtual void on_schedule(Struct_type *) = 0;
+	virtual void off_schedule(Struct_type *) = 0;
+	virtual void evict_one() = 0;
+};
+
+
+template <class Struct_type>
+class Scheduler_LRU : public Scheduler<Struct_type>
+{
+private:
+	const int max_size_;
+	std::list<Struct_type *> bookkeeping_; // most-LHS is most new
+
+public:
+	Scheduler_LRU(const int max_size)
+		: max_size_(max_size)
+		, bookkeeping_()
+	{}
+
+	bool is_full()
+	{
+		return bookkeeping_.size() >= max_size_;
+	}
+
+	bool is_hit(Struct_type *page)
+	{
+		auto it = std::find(bookkeeping_.begin(), bookkeeping_.end(), page);
+		return it != bookkeeping_.end();
+	}
+
+	void update(Struct_type *page)
+	{
+		if (!is_hit(page))
+		{
+			std::cerr << "Scheduler update bug warning/occur!\n";
+			return;
+		}
+
+		bookkeeping_.remove(page);
+		bookkeeping_.push_front(page);
+	}
+
+	void on_schedule(Struct_type *page)
+	{
+		bookkeeping_.push_front(page);
+	}
+
+	void off_schedule(Struct_type *page)
+	{
+		if (!is_hit(page))
+			std::cerr << "Scheduler off_schedule bug warning/occur!\n";
+
+		bookkeeping_.remove(page);
+	}
+
+	void evict_one()
+	{
+		bookkeeping_.pop_back();
+	}
+};
+
+#endif
---
+++ scheduler_test.hpp
@@ -0,0 +1,70 @@
+#ifndef __SCHEDULER_TEST_HPP__
+#define __SCHEDULER_TEST_HPP__
+
+#include "damemory.hpp"
+
+template <template <class> class Scheduler_type, class Struct_type>
+class Scheduler_test
+{
+private:
+	static const int max_size = 4;
+	static const int total_size = 6;
+	Scheduler<Struct_type> *scheduler;
+	Page *page[total_size];
+
+public:
+	void main()
+	{
+		scheduler = new Scheduler_type<Struct_type>(max_size);
+
+		for (int i = 0; i < total_size; i++)
+		{
+			std::string name = "tmp." + std::to_string(i);
+			page[i] = new Page(name);
+
+			if (!scheduler->is_full())
+			{
+				scheduler->on_schedule(page[i]);
+			}
+		}
+
+		std::cout << "** first max_size is hit\n";
+		check_hits(); // 3, 2, 1, 0
+
+		std::cout << "** page 3~1 is hit\n";
+		scheduler->evict_one(); // 3, 2, 1
+		check_hits();
+		std::cout << "** is_full " << scheduler->is_full() << "\n";
+
+		std::cout << "** page 5,3~1 is hit\n";
+		scheduler->on_schedule(page[5]); // 5, 3, 2, 1
+		check_hits();
+		
+		std::cout << "** warning test\n";
+		scheduler->off_schedule(page[0]);
+
+		std::cout << "** page 0,5,3,2 is hit\n";
+		scheduler->evict_one(); // 5, 3 ,2
+		scheduler->on_schedule(page[0]); // 0, 5, 3, 2
+		check_hits();
+
+		std::cout << "** page 2,0,5,3 is hit\n";
+		check_hits(); 
+		scheduler->update(page[2]);
+
+		std::cout << "** page 2,0,5 is hit\n";
+		scheduler->evict_one();
+		check_hits(); 
+	}
+
+	void check_hits()
+	{
+		for (int i = 0; i < total_size; i++)
+		{
+			std::cout << "page " + std::to_string(i) + " is_hit " << scheduler->is_hit(page[i]);
+			std::cout << "\n";
+		}
+	}
+};
+
+#endif
