8f273dbcfb43198bc77293f7dad22f9efc22c18a
thestyle <thestyle1202@gmail.com>
2014-06-16 20:11:13 +0800

    benchmark makefile update

--- benchmark/policy.sh
+++
@@ -1,4 +0,0 @@
-for file in $(ls *.cpp)
-do
-	sed "s/Arrranger_concatenating/Arranger_hyperpadding/g" $file > $file
-done
--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,7 +1,22 @@
 EXEC="left-looking_cholesky_factorization lu_decomposition matrix_chain_order multiplex optimal_bst right-looking_cholesky_factorization transpose wavelet_image_compression wavelet_image_decompression"
 
+echo "== [[ Padding ]] =="
 for EX in $EXEC
 do
 	echo "Run $EX"
-	time ./$EX
+	time padding/$EX
+done
+
+echo "== [[ Concatenating ]] =="
+for EX in $EXEC
+do
+	echo "Run $EX"
+	time concatenating/$EX
+done
+
+echo "== [[ Hyperpadding ]] =="
+for EX in $EXEC
+do
+	echo "Run $EX"
+	time hyperpadding/$EX
 done
